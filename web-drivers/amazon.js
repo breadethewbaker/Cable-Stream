dc66eeda847de4065346087717182d5f80df030e
Brett Baker <brettbaker@Bretts-MacBook-Air.local>
2020-12-09 14:34:39 -0500

    ambiguated amazon webdriver

--- web-drivers/amazon.js
+++ web-drivers/amazon.js
@@ -1,14 +1,16 @@
 const {Builder, By, Capabilities, Key, until} = require('selenium-webdriver');
 
+const title = "Pineapple Express"
+
 const driver = new Builder()
   .forBrowser('chrome')
   .build();
 //https://www.amazon.com/s?k=the+boys&i=prime-instant-video&ref=nb_sb_noss_1
 driver.get('https://www.amazon.com/Prime-Video/b?ie=UTF8&node=2676882011');
-driver.findElement(By.name('field-keywords')).sendKeys('twilight', Key.RETURN);
-driver.wait(until.titleIs('Amazon.com : twilight'), 20000).then(()=>{
-  driver.findElement(By.linkText('Twilight')).click();
+driver.findElement(By.name('field-keywords')).sendKeys(title, Key.RETURN);
+driver.wait(until.titleIs('Amazon.com : '+title), 20000).then(()=>{
+  driver.findElement(By.linkText(title)).click();
 });
-driver.wait(until.titleIs('Watch Twilight | Prime Video'), 20000).then(()=>{
+driver.wait(until.titleIs('Watch '+title+' | Prime Video'), 20000).then(()=>{
   driver.findElement(By.partialLinkText('Trailer')).click();
 });
